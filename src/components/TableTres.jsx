52629f41c2c15f7b3a5afc419f5276ca84fe70b3
FernDev <75585604+FernandoEscuderoDev@users.noreply.github.com>
2023-11-08 18:34:53 -0300

    a

--- src/components/TableTres.jsx
+++ src/components/TableTres.jsx
@@ -207,7 +207,7 @@ export default function TableTres() {
               </div>
             </div>
             <div className="z-10 flex h-16 w-full items-center justify-center rounded-b-xl gap-5 bg-purple-800 shadow-lg text-lg">
-              <a href="/" className="">
+              <a href="/tablesTres" className="">
                 Reintentar
               </a>
               <a href="/" className="">
